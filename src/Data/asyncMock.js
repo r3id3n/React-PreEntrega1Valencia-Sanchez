15f0debbf950d1364280e706b58d14aa0de1e6cd
Felipe Valencia <r3id3n2091@hotmail.com>
2024-04-17 21:02:50 -0400

    Update asyncMock.js

--- src/Data/asyncMock.js
+++ src/Data/asyncMock.js
@@ -5,7 +5,8 @@ const products = [
     price: 100000,
     category: "AMD",
     stock: 10,
-    img: 'https://media.solotodo.com/media/products/1580673_picture_1651582928.jpg'
+    img: '/src/Data/Photo/ryzen-5.jpg'
+    // img: 'https://media.solotodo.com/media/products/1580673_picture_1651582928.jpg'
   },
   {
     id: 2,
@@ -13,7 +14,8 @@ const products = [
     price: 200000,
     category: "Intel",
     stock: 5,
-    img: 'https://media.solotodo.com/media/products/1514024_picture_1640156807.jpg'
+    img: '/src/Data/Photo/intel-core-i5.jpg'
+    // img: 'https://media.solotodo.com/media/products/1514024_picture_1640156807.jpg'
   },
   {
     id: 3,
@@ -21,7 +23,8 @@ const products = [
     price: 300000,
     category: "AMD",
     stock: 3,
-    img: 'https://media.solotodo.com/media/products/1569342_picture_1672268351.jpg'
+    img: '/src/Data/Photo/ryzen-7.jpg'
+    // img: 'https://media.solotodo.com/media/products/1569342_picture_1672268351.jpg'
   },
 ];
 export const getProducts = () => {
